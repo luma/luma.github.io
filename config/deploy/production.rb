840dadc9c207a4da5ab2cb4d31b3746de5dd5e22
Rolly Fordham <rolly@luma.co.nz>
2015-10-08 21:08:09 -0700

    removing the old site

--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-set :branch, "production"
-
-role :web, "luma.co.nz"                           # Your HTTP server, Apache/etc
-role :app, "luma.co.nz"                           # This may be the same as your `Web` server
-role :db,  "luma.co.nz", :primary => true         # This is where Rails migrations will run
